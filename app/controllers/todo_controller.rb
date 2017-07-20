e2824fb808710c26e69d64e336133cf735bae99e
Philip Lu <philiplu97@gmail.com>
2017-07-20 14:31:21 +0000

    10:31 AM

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,15 @@
 class TodoController <ApplicationController
-    def index 
+    def index
+         @todo_description = "Eat breakfast" 
+        @todo_pomodoro_estimate = 1
     end
+    
     def show
+         
+    end
+    
+    def Edit
+    end
+    def Delete
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'todo/index', to: 'todo#index'
-  get 'todo/show', to: 'todo#show'
+  get 'todo/index/:id', to: 'todo#index'
+  get 'todo/show/:id', to: 'todo#show'
+  get 'todo/Edit', to: 'todo#Edit'
+  get 'todo/Delete', to: 'todo#Delete'
  end
 
 
