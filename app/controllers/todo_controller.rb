754e0649eec3d20400c086141fad6d37d7514ef5
Philip Lu <philiplu97@gmail.com>
2017-07-21 14:17:06 +0000

    Coding camp done :(

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,24 +1,35 @@
 class TodoController <ApplicationController
     def index
+        @todos = Todo.all
     end
     
     def show
         todo_id = params[:id]
         @todo = Todo.find_by_id(params[:id])
     end 
-    
+    def create
+        t =Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end    
     def edit
         @todo = Todo.find_by_id(params[:id])
         
     end
-    def update t = Todo.find_by_id(params[:id]) 
+    def update 
+        t = Todo.find_by_id(params[:id]) 
         t.description = params['description'] 
         t.pomodoro_estimate = params['pomodoro-estimate'] 
         t.save 
         redirect_to "/todo/show/#{t.id}" 
     end
     
-    def delete
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/"
     end
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'todo#index'
   get 'todo/show/:id', to: 'todo#show'
+  get 'todo/new', to: 'todo#new'
+  get 'todo/create', to: 'todo#create'
   get 'todo/edit/:id', to: 'todo#edit'
   get 'todo/update/:id', to: 'todo#update'
-  get 'todo/delete', to: 'todo#delete'
-  root to: 'todo#index'
+  get 'todo/Pinkguy', to: 'todo#Pinkguy'
+  get 'todo/LeOgre', to: 'todo#LeOgre'
+  get 'todo/REEEEEE', to: 'todo#REEEEEE'
+  get 'todo/Cory', to: 'todo#Cory'
+  get 'todo/h3h3', to: 'todo#h3h3'
+  get 'todo/Yeahbwoi', to: 'todo#Yeahbwoi'
+  get 'todo/destroy/:id', to: 'todo#destroy'
  end
 
 
